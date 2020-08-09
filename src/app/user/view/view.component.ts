c65dc50a1a33fd53543e6a57679dc690f31c7efd
Neven Klofutar <neven.klofutar@gmail.com>
2020-08-09 12:13:42 +0200

    Updated view component

--- src/app/user/view/view.component.ts
+++ src/app/user/view/view.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { routes } from 'src/app/auth/auth-routing.module';
+import { User, Post } from 'src/backend/interfaces';
+import { UserService } from 'src/backend/endpoints/user.service';
+import { AlertifyService } from 'src/app/shared/_services/alertify.service';
+import { PostService } from 'src/backend/endpoints/post.service';
 
 @Component({
     selector: 'app-view',
@@ -8,12 +11,40 @@ import { routes } from 'src/app/auth/auth-routing.module';
     styleUrls: ['./view.component.css'],
 })
 export class ViewComponent implements OnInit {
-    constructor(private route: ActivatedRoute) {
+    userId: string;
+    user: User;
+    posts: Post[];
+
+    constructor(
+        private route: ActivatedRoute,
+        private userService: UserService,
+        private postService: PostService,
+        private alertifyService: AlertifyService
+    ) {
         this.route.queryParams.subscribe((params) => {
-            let id = params['id'];
-            console.log(id);
+            this.userId = params['id'];
+            // console.log(id);
         });
     }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.userService.getUserById(this.userId).subscribe(
+            (response) => {
+                this.user = response;
+                console.log(this.user);
+            },
+            (error) => {
+                this.alertifyService.error(error.error.title);
+            }
+        );
+
+        this.postService.getPostsForUser(this.userId).subscribe(
+            (response) => {
+                this.posts = response;
+            },
+            (error) => {
+                this.alertifyService.error(error.error.title);
+            }
+        );
+    }
 }
