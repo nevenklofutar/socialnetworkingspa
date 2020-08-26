471afd3dc153a7cd45275af93ea8a003ec04c864
Neven Klofutar <neven.klofutar@gmail.com>
2020-08-26 17:50:02 +0200

    Fixed Search and View components

--- src/app/user/search-list/search-list.component.ts
+++ src/app/user/search-list/search-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { User } from '../../../backend/interfaces';
+import { AuthService } from 'src/backend/endpoints/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-search-list',
@@ -8,8 +10,20 @@ import { User } from '../../../backend/interfaces';
 })
 export class SearchListComponent implements OnInit {
     @Input() users: User[];
+    currentUser: User;
 
-    constructor() {}
+    constructor(private authService: AuthService, private router: Router) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.currentUser = this.authService.getCurrentUser();
+    }
+
+    redirectToUserPage(user: User) {
+        if (user.id === this.currentUser.id)
+            this.router.navigate(['/user/main']);
+        else
+            this.router.navigate(['/user/view'], {
+                queryParams: { id: user.id },
+            });
+    }
 }
